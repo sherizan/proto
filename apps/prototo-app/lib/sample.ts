e5f4e27b3f17973bd15920ebdd7de7041e886fe5
Sheri <sherizan@gmail.com>
2026-06-22 16:41:27 -0700

    feat(viewer): pin the sample prototype deep link

---
+++ apps/prototo-app/lib/sample.ts
@@ -0,0 +1,5 @@
+export const SAMPLE = {
+  name: 'Sample prototype',
+  deepLink:
+    'prototo://expo-development-client/?url=https://u.expo.dev/8c8ddf7d-1f6a-4b21-a7cc-116ec4d72c6d/group/6771d994-cc56-4777-ace1-1ec711d1261a',
+};
